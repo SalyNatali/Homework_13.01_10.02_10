5b8c7069be7087b4f9d5e301d3e781b197074678
Natali <natali.salyanova@yandex.ru>
2023-01-19 19:11:56 +0300

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Например:
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+Console.WriteLine("Чётные числа от 1 до num");
+
+while (x < num)
+{
+    if (x % 2 == 0)
+    Console.WriteLine($"{x},");
+    x++;
+}
+
+
+
+
